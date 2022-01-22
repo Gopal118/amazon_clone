751064dc8a29f5152a2fe8953b9e63e434765916
Gopal <gopal.sabadotechnologies@gmail.com>
2022-01-23 00:28:47 +0530

    addded new compononts.

--- amazon-clone_client/src/app/app.module.ts
+++ amazon-clone_client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
